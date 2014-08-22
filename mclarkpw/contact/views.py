488594b7bc54dc61c61c819472dcc807d6b6ffe3
Michael Clark <michael.clark@puritynine.com>
2014-08-21 23:27:43 -0600

    Added maximum message limit
    Disabled the form when a configurable maximum number of messages
    has been reached (in the last 24 hrs).

--- mclarkpw/contact/views.py
+++ mclarkpw/contact/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 from django.core.urlresolvers import reverse
 from .forms import ContactForm
+from .models import Contact
 
 class ContactMainView(FormView):
     template_name="contact/main.html"
@@ -12,9 +13,17 @@ class ContactMainView(FormView):
         return reverse('contact.sent')
     
     def form_valid(self, form):
-        form.send_message()
+        if Contact.can_send():
+            form.send_message()
         return super(ContactMainView, self).form_valid(form)
     
+    def get_context_data(self, **kwargs):
+        context = super(ContactMainView, self).get_context_data(**kwargs)
+        
+        # Values to be loaded dynamically
+        context['can_send'] = Contact.can_send()
+        
+        return context
 
 class ContactSentView(TemplateView):
     template_name="contact/sent.html"
