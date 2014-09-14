5516f3fc86e357fe3b61c997428193c2d16287b6
Michael Clark <michael.clark@puritynine.com>
2014-09-14 14:55:25 -0600

    Added School, Position, and Tag models
    Added School, Position, and Tag models to the about app.  Also
    generated south migrations.

--- mclarkpw/about/models.py
+++ mclarkpw/about/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
+class Tag(TimeStampedModel):
+    name = models.CharField(max_length=64)
+
+class Position(TimeStampedModel):
+    company = models.CharField(max_length=128)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True)
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    
+class School(TimeStampedModel):
+    program = models.CharField(max_length=128)
+    school = models.CharField(max_length=128)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag)    
+    
