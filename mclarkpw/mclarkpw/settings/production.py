40c88de5c93f165458c3b83f82efe6b20911cf3e
Michael Clark <michael.clark@puritynine.com>
2014-09-19 00:14:27 -0600

    Updated production settings, wsgi

---
+++ mclarkpw/mclarkpw/settings/production.py
@@ -0,0 +1,19 @@
+import os
+from .base import *
+
+dbname = os.environ['MCLARKPW_DBNAME']
+dbuser = os.environ['MCLARKPW_DBUSER']
+dbpass = os.environ['MCLARKPW_DBPASS']
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+SECRET_KEY = os.environ['MCLARKPW_SECRET_KEY']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': dbname,
+        'USER': dbuser,
+        'PASSWORD': dbpass
+    }
+}
